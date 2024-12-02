443872831390589c1f4901c0c464b57299431dd7
[SkySnkrs] <[skysnkrs@gmail.com]>
2024-12-02 14:01:38 -0700

    init , added image

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+// JS
+<script setup>
+import { computed, ref } from 'vue'
+import { AppState } from './AppState.js'
+
+const cheese = ref(0)
+
+</script>
+
+
+// HTML
+<template>
+<main class="container-fluid">
+  <section class="row">
+    <div class="col-12 text-center my-3">
+      <img src="/src/assets/img/moon.png" alt="Image Of Moon"> 
+    </div>
+       
+  </section>
+</main>
+</template>
+
+
+// styles
+<style lang="scss">
+@import "./assets/scss/main.scss";
+
+img{
+  height: 20em;
+  aspect-ratio: 1/1;
+  text-shadow: 2px 2px red;
+}
+
+main{
+  background-color: rgb(40, 40, 40);
+}
+</style>
